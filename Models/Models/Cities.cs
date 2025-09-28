27b8029860a30372d392ddd7721ff054b8196953
Owner <Owner@660084-COMPN-09>
2025-09-28 11:11:35 +0300

    Add project files.

---
+++ Models/Models/Cities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Cities
+    {
+        int cityId;
+        string cityName;
+
+        public int CityId
+        {
+            get { return cityId; }
+            set { cityId = value; }
+        }
+        [Required(ErrorMessage = "You must enter city name")]
+        public string CityName
+        {
+            get { return cityName; }
+            set { cityName = value; }
+        }
+    }
+}
---
+++ Models/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        string userName;
+        string userEmail;
+        string userPassword;
+        string userAddress;
+        int cityId;
+        string userPhoneNumber;
+
+        [Required(ErrorMessage = "You must enter your name")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "First name cannot be longer than 15 characters and less than 2")]
+        [FirstLetterCapital(ErrorMessage = "First letter must be capital")]
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        [Required(ErrorMessage = "You must enter your email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string UserEmail
+        {
+            get { return userEmail; }
+            set { userEmail = value; }
+        }
+        [Required(ErrorMessage = "You must enter your password")]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Password must be between 4 and 25 characters.")]
+        [RegularExpression(@"^(?=.*\d)", ErrorMessage = "Password must contain at least one number.")]
+        public string Password
+        {
+            get { return userPassword; }
+            set { userPassword = value; }
+        }
+        [Required(ErrorMessage = "You must enter your address")]
+        public string UserAddress
+        {
+            get { return userAddress; }
+            set { userAddress = value; }
+        }
+        [Required(ErrorMessage = "You must enter your phone number")]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        public string UserPhoneNumber
+        {
+            get { return userPhoneNumber; }
+            set { userPhoneNumber = value; }
+        }
+        [Required(ErrorMessage = "You must enter your city")]
+        public int CityId
+        {
+            get { return cityId; }
+            set { cityId = value; }
+        }
+    }
+}
