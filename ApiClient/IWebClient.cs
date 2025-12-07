84de42bbd9c4b856b4e4d27f9a510f74e12e576f
Owner <Owner@660084-COMPN-09>
2025-12-07 11:19:09 +0200

    Clanker

---
+++ ApiClient/IWebClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    internal interface IWebClient<T>
+    {
+
+        //syncrocnous methods
+        T Get();
+        Task<T> GetAsync();
+        bool Post(T data);
+        bool Post(T data, FileStream file);
+        bool Post(T data, List<FileStream> file);
+        //asyncronous 
+        Task<T> PostAsync(T data);
+        Task<T> PostAsync(T data, FileStream file);
+        Task<bool> PostAsync(T data, List<FileStream> file);
+
+    }
+}
---
+++ ApiClient/WebClient.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    internal class WebClient<T> : IWebClient<T>
+    {
+        HttpClient httpClient;
+        HttpRequestMessage requestMessage;
+        HttpResponseMessage responseMessage;
+
+        UriBuilder uriBuilder; // מרכיב כתובת של בקשה 
+        public WebClient()
+        {
+            this.httpClient = new HttpClient();
+            this.uriBuilder = new UriBuilder();
+        }
+        public string Schema
+        {
+            set
+            {
+                this. uriBuilder.Scheme = value;
+            }
+        }
+        public string Host
+        {
+            set
+            {
+                this. uriBuilder.Host = value;
+            }
+        }
+        public int Port
+        {
+            set
+            {
+                this. uriBuilder.Port = value;
+            }
+        }
+        public string Path
+        {
+            set
+            {
+                this. uriBuilder.Path = value;
+            }
+        }
+        public void AddParameter(string key, string value)
+        {
+            if (this.uriBuilder.Query != string.Empty)
+                this.uriBuilder.Query += "&" + key + "=" + value;
+            else
+                this.uriBuilder.Query += key + "=" + value;
+        }
+        public T Get()
+        {
+            using (HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, this.uriBuilder.Uri))
+            {
+                requestMessage.Method = HttpMethod.Get;// get שיטת שליחת בקשה
+                requestMessage.RequestUri = this.uriBuilder.Uri; // מגדיר את כתובת הבקשה
+                using (HttpResponseMessage respondMessage = this.httpClient.SendAsync(requestMessage).Result) //בעזרתhttpclient שולחים בקשה לשרת
+                {
+                    if (responseMessage.IsSuccessStatusCode == true)
+                    {
+                        string result = responseMessage.Content.ReadAsStringAsync().Result;
+                        T data = JsonSerializer.Deserialize<T>(result);
+                        return data;
+                    }
+                    else
+                        return default(T);
+                }
+            }
+        }
+        public async Task<T> GetAsync()
+        {
+            using (HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, this.uriBuilder.Uri))
+            {
+                requestMessage.Method = HttpMethod.Get;// get שיטת שליחת בקשה
+                requestMessage.RequestUri = this.uriBuilder.Uri; // מגדיר את כתובת הבקשה
+                using (HttpResponseMessage respondMessage = await this.httpClient.SendAsync(requestMessage)) //בעזרתhttpclient שולחים בקשה לשרת
+                {
+                    if (responseMessage.IsSuccessStatusCode == true)
+                    {
+                        string result = await responseMessage.Content.ReadAsStringAsync();
+                        T data = JsonSerializer.Deserialize<T>(result);
+                        return data;
+                    }
+                    else
+                        return default(T);
+                }
+            }
+        }
+
+        public bool Post(T data)
+        {
+            using (HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, this.uriBuilder.Uri))
+            {
+                requestMessage.Method = HttpMethod.Post;// post שיטת שליחת בקשה
+                requestMessage.RequestUri = this.uriBuilder.Uri; // מגדיר את כתובת הבקשה
+                string jsonData = JsonSerializer.Serialize(data);
+                requestMessage.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                using(HttpResponseMessage responseMessage = this.httpClient.SendAsync(requestMessage).Result) 
+                    return responseMessage.IsSuccessStatusCode;
+            }
+        }
+
+        public bool Post(T data, List<FileStream> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Post(T data, FileStream file)
+        {
+            using (HttpRequestMessage requestMessage = new HttpRequestMessage())
+            {
+                requestMessage.Method = HttpMethod.Post;// post שיטת שליחת בקשה
+                requestMessage.RequestUri = this.uriBuilder.Uri; // מגדיר את כתובת הבקשה
+                MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+                string jsonData = JsonSerializer.Serialize(data);
+                StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                multipartFormDataContent.Add(stringContent);
+            }
+        }
+
+        public Task<T> PostAsync(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> PostAsync(T data, FileStream file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PostAsync(T data, List<FileStream> file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
